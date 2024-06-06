265332b354d25e3a11d381ddeeef193ee621ac9d
Lawrence Tsungu <tsungulawrence@gmail.com>
2024-06-06 12:07:39 +0300

    code

--- customers.js
+++ customers.js
@@ -1,42 +1,3 @@
-// document.addEventListener('DOMContentLoaded', async function () {
-//     const tableBody = document.querySelector('#customer-table tbody');
-
-//     try {
-//         const response = await fetch('https://offering.pockethost.io/api/collections/Water/records');
-//         if (response.ok) {
-//             const data = await response.json();
-//             data.items.forEach(customer => {
-//                 const row = document.createElement('tr');
-
-//                 const nameCell = document.createElement('td');
-//                 nameCell.textContent = customer.name;
-//                 row.appendChild(nameCell);
-
-//                 const emailCell = document.createElement('td');
-//                 emailCell.textContent = customer.email;
-//                 row.appendChild(emailCell);
-
-//                 const phoneCell = document.createElement('td');
-//                 phoneCell.textContent = customer.phone;
-//                 row.appendChild(phoneCell);
-
-//                 const addressCell = document.createElement('td');
-//                 addressCell.textContent = customer.address;
-//                 row.appendChild(addressCell);
-
-//                 tableBody.appendChild(row);
-//             });
-//         } else {
-//             const error = await response.json();
-//             console.error('Error:', error);
-//         }
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// });
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const tableBody = document.querySelector('#customer-table tbody');
 
@@ -45,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const response = await fetch('https://offering.pockethost.io/api/collections/Water/records');
             if (response.ok) {
                 const data = await response.json();
-                tableBody.innerHTML = ''; // Clear existing rows
+                tableBody.innerHTML = ''; // Clear existing rows 1st before refetching from my pockethost 
                 data.items.forEach(customer => {
                     const row = document.createElement('tr');
 
